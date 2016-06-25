48e62160dd4dc957aae6285cf4a96bc11ba85df3
Roquerg <jroqueromang@hotmail.com>
2016-06-25 13:22:47 -0500

    Clase UI y Sistemas de partículas
    Avances de lo visto en clase

---
+++ Assets/codigos/carga.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+
+
+public class carga : MonoBehaviour {
+	public bool abrir,aparece;
+    public GameObject descripcion,botonInfo;
+	public Image vida;
+	public Text leyenda;
+	public float proporcion;
+	// Use this for initialization
+	void Start () {
+		abrir = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (abrir)
+		EditorSceneManager.LoadScene (0);
+		descripcion.SetActive (aparece);
+		botonInfo.SetActive (!aparece);
+		leyenda.text =proporcion.ToString();
+		vida.fillAmount = proporcion;
+	}
+
+	//public static void LoadScene(int , SceneManagement.LoadSceneMode mode = LoadSceneMode.Single);
+	public void cargaScena(){
+		abrir = true;
+	}
+
+	public void describe(){
+		aparece = !aparece;
+
+	}
+
+}
---
+++ Assets/codigos/particulas.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particulas : MonoBehaviour {
+	public ParticleSystem fuego;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.S))
+		fuego.Play ();
+
+		if (Input.GetKey (KeyCode.A))
+			fuego.Stop ();
+	}
+}
